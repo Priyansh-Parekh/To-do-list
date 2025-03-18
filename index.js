0bc20b2f66a32abba591143b3b288486b4485f44
Priyansh-Parekh <priyanshparekh24@gmail.com>
2025-03-18 23:38:25 +0530

    my first Backend project

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const fs = require('node:fs')
+
+
+app.set("view engine", "ejs")
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, "public")))
+
+app.get("/", (req, res) => {
+    fs.readdir(`./file`, (err, file) => {
+        if (err) {
+            console.error("Error reading directory:", err);
+            return res.status(500).send("Internal Server Error");
+        }
+
+        res.render("index", { files : file }); 
+    })
+
+})
+
+app.post("/create",(req,res)=>{
+    fs.writeFile(`./file/${req.body.title.split(" ").join("")}.txt`,req.body.details,(err)=>{
+        if (err) {
+            console.error("Error writing file:", err);
+            return res.status(500).send("Error saving the task");
+        }
+        res.redirect("/")
+    })
+});
+
+app.get("/file/:filename",(req,res)=>{
+    fs.readFile(`./file/${req.params.filename}`,"utf-8",(err,data)=>{
+        res.render('show',{data:data,filename:req.params.filename},);
+    })
+});
+// console.log("hello")
+
+app.listen(3000)
